347d6a4ad84f53cec032a06652c86a1dc45f1b4e
Diego Fernando Masutti Galetto <dmasutti@securitasargentina.com>
2019-09-18 10:03:34 -0300

    login con google y firebase

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,42 @@
-import React from 'react';
 import './Login.css';
+import React, { Component } from 'react';
+import whithFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../firebaseConfig';
 
-class Login extends React.Component {
-    constructor(props) {
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = { googleProvider: new firebase.auth.GoogleAuthProvider() };
+
+class Login extends Component
+{
+    constructor(props){
         super(props);
+        this.props = props;
     }
 
-    render() {
+    render(){
+        const {user, signOut, signInWithGoogle} = this.props;
+        
         return (
-
-            <div className="text-center">
-                <form className="form-signin">
-                    <img className="mb-4" src={require('../../images/barco-que-hace-aguas.gif')} alt="" width="250" height="250" />
-                    <h1 className="h3 mb-3 font-weight-normal">Iniciar sesión</h1>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Correo electrónico" required />
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Contraseña" required />
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">Iniciar</button>
-                </form>
+            <div className="text-center">      
+                <h1>Batalla Naval</h1>
+                <img className="mb-4" src={require('../../images/barco-que-hace-aguas.gif')} alt="" width="250" height="250" />
+                <div>
+                {
+                    user ? <h4>Bienvenido, {user.displayName}</h4> : <h4>Iniciar Sesión</h4> 
+                }
+                </div>
+                <div>    
+                {
+                    user ? <button className="btn btn-lg btn-primary btn-block btn-signin" onClick={signOut}>Salir</button>
+                         : <button className="btn btn-lg btn-primary btn-block btn-signin" onClick={signInWithGoogle}>Ingresar con google</button>
+                }
+                </div>
             </div>
         );
     }
 }
 
-export default Login;
+export default whithFirebaseAuth({ providers, firebaseAppAuth }) (Login);
