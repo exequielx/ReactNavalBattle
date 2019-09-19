a265ef5cc807deba5e76e37ea5c25b2a7e47496b
Diego Fernando Masutti Galetto <dmasutti@securitasargentina.com>
2019-09-19 09:38:23 -0300

    aaaa

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyAwA-jbfPbMPKkcgbTvVw27ohbwkTRgoH8",
-    authDomain: "reactnavalbattle.firebaseapp.com",
-    databaseURL: "https://reactnavalbattle.firebaseio.com",
-    projectId: "reactnavalbattle",
-    storageBucket: "",
-    messagingSenderId: "799200679695",
-    appId: "1:799200679695:web:aff28b49d7d0006088107a"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
